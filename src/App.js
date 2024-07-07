6436d7a04c7e64b22c3fc534143b04e9667bb212
Harun Gundogan <146117743+Harun-Gundogan@users.noreply.github.com>
2024-07-07 20:34:25 +0300

    version2
    Detay sayfası eklendi.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import IlanList from './components/IlanList';
+import IlanDetay from './pages/IlanDetay';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-      <h1>İş İlan Portalı</h1>
-      <div className="header">
-
-        <div className="arama_kutusu">
-          <input type="text" placeholder="İş ilanı Arayın"/>
-          <button id="arama_butonu">Ara</button>
+    <Router>
+      <div className="App">
+        <h1>İş İlan Portalı</h1>
+        <div className="header">
+          <div className="arama_kutusu">
+            <input type="text" placeholder="İş ilanı Arayın" />
+            <button id="arama_butonu">Ara</button>
+          </div>
         </div>
+        <Routes>
+          <Route path="/" element={<IlanList />} />
+          <Route path="/ilan/:id" element={<IlanDetay />} />
+        </Routes>
       </div>
-      <IlanList/>
-    </div>
+    </Router>
   );
 }
 
